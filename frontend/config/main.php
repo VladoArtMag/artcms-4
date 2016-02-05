18ad2769494950e2b8bb20c7ccb814abc9e1528a
vlado <sfera66@bk.ru>
2016-02-05 19:17:54 +0200

    yii2-user

---
+++ frontend/config/main.php
@@ -0,0 +1,38 @@
+<?php
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
+);
+
+return [
+    'id' => 'app-frontend',
+    'basePath' => dirname(__DIR__),
+    'homeUrl' => '/',
+    'bootstrap' => ['log'],
+    'controllerNamespace' => 'frontend\controllers',
+    'components' => [
+        'request' => [
+            'baseUrl' => '',
+        ],
+        'user' => [
+            'identityClass' => 'budyaga\users\models\User',
+            'enableAutoLogin' => true,
+            'loginUrl' => ['/login'],
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+    ],
+    'params' => $params,
+];
